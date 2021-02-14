80214bca489c8dcdc6cc905cb329f5fd4babcae8
Kayra <kayrabtuncer@gmail.com>
2021-02-14 16:05:03 +0300

    Finish All Routes

--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -2,53 +2,84 @@ const express = require('express')
 const router = express.Router()
 router.use(express.json())
 
+const db = require('../db')
+
 // Get All Restaurants
-router.get('/', (req, res) => {
-    res.status(200).json({
-        data: {
-            restaurant: ["burgerking", "starbucks"]
-        }
-    })
+router.get('/', async (req, res) => {
+
+    try {
+        const results = await db.query("select * from restaurants")
+        res.status(200).json({
+            results: results.rows.length,
+            data: {
+                restaurant: results.rows
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
 })
 
 // Get Single Restaurant
-router.get('/:id', (req, res) => {
-    console.log(req.params)
+router.get('/:id', async (req, res) => {
 
-    res.status(200).json({
-        data: {
-            restaurant: "burgerking"
-        }
-    })
+    try {
+        const results = await db.query("select * from restaurants where id = $1", [req.params.id])
+        res.status(200).json({
+            data: {
+                restaurant: results.rows[0]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 // Create Restaurant
-router.post('/', (req, res) => {
-    console.log(req.body)
+router.post('/', async (req, res) => {
 
-    res.status(201).json({
-        data: {
-            restaurant: "burgerking"
-        }
-    })
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
+            [req.body.name, req.body.location, req.body.price_range])
+        res.status(201).json({
+            data: {
+                restaurant: results.rows[0]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 // Update Restaurant
-router.put('/:id', (req, res) => {
-    console.log(req.params.id)
-    console.log(req.body)
+router.put('/:id', async (req, res) => {
 
-    res.status(200).json({
-        data: {
-            restaurant: "burgerking"
-        }
-    })
+    try {
+        const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+            [req.body.name, req.body.location, req.body.price_range, req.params.id])
+        res.status(200).json({
+            data: {
+                restaurant: results.rows[0]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    res.status(204).json({
-        message: "success"
-    })
+// Delete Restaurant
+router.delete('/:id', async (req, res) => {
+
+    try {
+        const results = await db.query("DELETE FROM restaurants WHERE id = $1 returning *",
+            [req.params.id])
+        res.status(204).json({
+            message: "success"
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = router
