4885882a5c5da92202a874afd313f758935c95c2
Kayra <kayrabtuncer@gmail.com>
2021-02-15 19:28:18 +0300

    Add context

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './routes/Home'
 import UpdatePage from './routes/UpdatePage'
 import DetailPage from './routes/DetailPage'
+import { RestaurantsContextProvider } from './context/RestaurantContext'
 
 const App = () => {
-    return <div className="container">
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/restaurants/:id/update" component={UpdatePage} />
-                <Route exact path="/restaurants/:id" component={DetailPage} />
-            </Switch>
-        </Router>
-    </div>
+    return (
+        <RestaurantsContextProvider>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+                        <Route exact path="/restaurants/:id" component={DetailPage} />
+                    </Switch>
+                </Router>
+            </div>
+        </RestaurantsContextProvider>
+    )
 }
 
 export default App
